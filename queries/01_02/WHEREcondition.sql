5f8edb89d649f7a63f1436dc6a28733e7dbfed00
alexTUML <159502579+alexTUML@users.noreply.github.com>
2024-03-06 08:29:26 +0000

    Practice with WHERE condition

--- queries/01_02/WHEREcondition.sql
+++ queries/01_02/WHEREcondition.sql
@@ -1,7 +1,10 @@
 SELECT empno,name,salary 
 FROM emp_tab 
 WHERE salary>2500;
-
+/* retrieves info (empno, name, salary) for all employees from
+emp_tab that have a greater salary than 2500*/
 SELECT empno,name, salary 
 FROM emp_tab 
 WHERE name='KING';
+/*retrieves info for any employee that has the name 'KING'
+has to be enclosed in single quotes '' in order for WHERE to work*/
