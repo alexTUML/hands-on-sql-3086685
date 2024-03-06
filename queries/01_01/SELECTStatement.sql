c5bb3e2be7a037b6298411174447fa3b382da2a7
alexTUML <159502579+alexTUML@users.noreply.github.com>
2024-03-06 07:45:27 +0000

    Practice for fetching data

--- queries/01_01/SELECTStatement.sql
+++ queries/01_01/SELECTStatement.sql
@@ -2,4 +2,7 @@ SELECT * FROM emp_tab;
 
 SELECT empno,name FROM emp_tab;
 
-SELECT empno,name,salary*12 'Annual Salary' FROM emp_tab;
+/* if using just one word for a category name, then no need for quote marks
+but if using more than one, use quote marks such as ''*/
+
+SELECT empno,name,salary*12  'Annual Salary' FROM emp_tab;
